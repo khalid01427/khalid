46ed204974329238edb366773f394a65d5ff0f19
KHALID ANWAR <KHALID ANWAR@DESKTOP-U889HR5>
2018-09-03 18:16:55 +0530

    using for first Time

---
+++ IMT/src/MainController/GivenByDeloper.java
@@ -0,0 +1,11 @@
+package MainController;
+
+public class GivenByDeloper {
+	
+	public int add(int a,int b)
+	{
+		int c=a+b;
+		return c;
+	}
+
+}
---
+++ IMT/src/MainController/Inspection.java
@@ -0,0 +1,20 @@
+package MainController;
+
+import org.testng.annotations.Test;
+
+public class Inspection {
+  @Test
+  public void addfunctionality() {
+	  GivenByDeloper obj=new GivenByDeloper();
+	  int sum=0;
+	  sum=obj.add(10, 20);
+	  System.out.println(sum);
+  }
+  @Test
+  public void addfunctionality2() {
+	  GivenByDeloper obj=new GivenByDeloper();
+	  int sum=0;
+	  sum=obj.add(2147483647, 2147483647);
+	  System.out.println(sum);
+  }
+}
